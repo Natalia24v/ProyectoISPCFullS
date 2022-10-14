94d4789403d1221e5c4cdc573e4ed967ad6a722b
Brian Goldy <106565462+BrianGoldy@users.noreply.github.com>
2022-10-13 23:10:52 -0300

    Parte visual del mvc
    Seguir agregando contenido

---
+++ PaginaInicio.py
@@ -0,0 +1,4 @@
+#Mensaje de bienvenida al usuario
+#Cuadro de login(Que contenga usuario y contraseña) mas un boton de para enviar datos
+#Imagen,logo o nombre que represente a la aplicacion.
+#Informacion de la aplicacion como por ejemplo explicacion de su utilidad, informacion de los desarrolladores, como funciona y como se utiliza
