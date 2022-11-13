fab0abb38b25d5fb3d3ee9f50fa7b6f79e3e170a
Brian Goldy <106565462+BrianGoldy@users.noreply.github.com>
2022-11-13 12:11:46 -0300

    Update crudCitasYa2.py

--- Backend/crudCitasYa2.py
+++ Backend/crudCitasYa2.py
@@ -17,13 +17,13 @@ class conexion():
             print("Error en la conexion")
 
     ## Insertar informacion personal para login
-    def insertarValores(self,id_usuario,nombres,apellido,edad,genero,interes,bio_personal,limite_distancia,id_locacion,rango_edad):
+    def insertarValores(self):
         if self.conexion.is_connected():
             try:
                 cursor=self.conexion.cursor()
-                insertar="INSERT INTO Usuario VALUES('0001','John','Osbourne','40','M','Sin definir','Sin Bio','15Km','5001','35-45')"
-                data=(id_usuario,nombres,apellido,edad,genero,interes,bio_personal,limite_distancia,id_locacion,rango_edad)
-                cursor._executed(insertar,data)
+                insertar="INSERT INTO usuario(id_usuario,nombres,apellido,edad,genero,interes,bio_personal,limite_distancia,id_locacion,rango_edad) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+                data=("0001","John","Osbourne","35","M","Sin especificar","músico y compositor británico","20KM","5000","30-40")
+                cursor.executed(insertar,data)
                 self.conexion.commit()
                 self.conexion.close()
             except:
@@ -35,9 +35,9 @@ class conexion():
             try:
                 cursor = self.conexion.cursor()
                 cursor.execute("SELECT * FROM unmatch")
-                resultados = cursor.fetchall()
-                print(resultados)
-                return resultados
+                for fila in cursor:
+                    print(fila)
+                conexion.close()
             except:
                 print("Error en la conexion")
 ##Buscar contactos match1
@@ -45,14 +45,15 @@ class conexion():
         if self.conexion.is_connected():
             try:
                 cursor=self.conexion.cursor()
-                select="SELECT * from unmatch"
+                select="SELECT id_usuario from match1"
                 cursor.execute(select)
-                resultado=cursor.fetchall()
-                self.conexion.close()
-                return resultado
+                for fila in cursor:
+                    print(fila)
+                conexion.close()
 
             except:
                 print("Error en la conexion")
+                
     ## Eliminar Contacto con match
     def eliminarContacto(self):
         if self.conexion.is_connected():
