82fd85a92b190d4e0acff42e17ea5ade7f3511de
Natalia <natyruiz2017@gmail.com>
2023-05-06 16:59:40 -0300

    componentes

--- aplicación/primeraApp/src/app/app.module.ts
+++ aplicación/primeraApp/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { NavComponent } from './layout/nav/nav.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavComponent,
+    DashboardComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
